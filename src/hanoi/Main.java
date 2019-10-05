23265a097103c7495f6306082c070ed91cbf2054
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-05 21:00:50 +0530

    input variables:numberOfDisks(int),from(char),helper(char),to(char)

--- src/hanoi/Main.java
+++ src/hanoi/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int numberOfDisks = Integer.parseInt(scanner.nextLine());
+        char from = scanner.next().charAt(0);
+        char helper = scanner.next().charAt(0);
+        char to = scanner.next().charAt(0);
         scanner.close();
     }
 }
