dda11488aa6bd65b6fdd5b144afcd8c078925e40
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-05 20:43:30 +0530

    add getter & setter methods

--- src/hanoi/Hanoi.java
+++ src/hanoi/Hanoi.java
@@ -6,4 +6,35 @@ public class Hanoi {
     private char from;
     private char to;
 
+    public int getNumberOfDisks() {
+        return numberOfDisks;
+    }
+
+    public void setNumberOfDisks(int numberOfDisks) {
+        this.numberOfDisks = numberOfDisks;
+    }
+
+    public char getHelper() {
+        return helper;
+    }
+
+    public void setHelper(char helper) {
+        this.helper = helper;
+    }
+
+    public char getFrom() {
+        return from;
+    }
+
+    public void setFrom(char from) {
+        this.from = from;
+    }
+
+    public char getTo() {
+        return to;
+    }
+
+    public void setTo(char to) {
+        this.to = to;
+    }
 }
