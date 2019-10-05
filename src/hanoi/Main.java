5e38b74892ef8f0f1f661723e654ee03dcdd7c75
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-10-05 20:58:29 +0530

    add object:scanner of class:Scanner

--- src/hanoi/Main.java
+++ src/hanoi/Main.java
@@ -1,7 +1,10 @@
 package hanoi;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        scanner.close();
     }
 }
